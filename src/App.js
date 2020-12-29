d28f7d8e10156b4ad5c6de14a95a1ed3e5bc6af8
Kendrick Tan <kendricktan0814@gmail.com>
2020-12-29 10:12:44 +1000

    minus 3

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
             "https://api-esd.oca.wtf/data/ESD-DAO.json"
           ).then((x) => x.json());
 
-          const { tree, bar } = formatDaoStats(daoData, epoch);
+          const { tree, bar } = formatDaoStats(daoData, epoch - 3);
 
           setDaoStats(daoData);
           setDaoChartData(bar);
@@ -71,7 +71,7 @@ function App() {
             "https://api-esd.oca.wtf/data/ESD-LP.json"
           ).then((x) => x.json());
 
-          const { tree, bar } = formatLpStats(lpData, epoch);
+          const { tree, bar } = formatLpStats(lpData, epoch - 3);
 
           setLpStats(lpData);
           setLpTreeValue(tree);
